1643fc85f4457f9d24e7d88a2bb8d930665e31df
Kim So Yeon <77036230+iolite-ruru@users.noreply.github.com>
2022-02-05 20:09:08 +0900

    Create [4차] 사전에서 단어찾기.c

---
+++ 구름에듀/[4차] 사전에서 단어찾기.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LEN (3906)
+char* vowels[5] = {"A", "E", "I", "O", "U"};
+char* words[3906];
+int words_idx = 0;
+
+
+void create_words(int lev, char* str) {
+    words[words_idx++] = str;
+    for (int i = 0; i < 5; i++) {
+        if (lev < 5) {
+            char* new_str = (char*)malloc(sizeof(char)*(lev+2));
+            strcpy(new_str, str);
+            strcat(new_str, vowels[i]);
+            create_words(lev, new_str);
+        }
+    }
+}
+
+int solution(char* word) {
+    int answer = 0;
+    words_idx = 0;
+    create_words(0, "");
+    for (int i = 0; i < words_idx; i++)
+        if (strcmp(word, words[i]) == 0) {
+            answer = i;
+            break;
+        }
+    return answer;
+}
+
+int main() {
+    char* word1 = "AAAAE";
+    int ret1 = solution(word1);
+
+    printf("solution 함수의 반환 값은 %d 입니다.\n", ret1);
+
+    char* word2 = "AAAE";
+    int ret2 = solution(word2);
+
+    printf("solution 함수의 반환 값은 %d 입니다.\n", ret2);
+    return 0;
+}
