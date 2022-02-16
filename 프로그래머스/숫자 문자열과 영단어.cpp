99156db2d78c5f1de63bddcb2f48d410d4b0c461
Kim So Yeon <s2023@e-mirim.hs.kr>
2022-02-16 20:51:23 +0900

    Update 숫자 문자열과 영단어.cpp

--- 프로그래머스/숫자 문자열과 영단어.cpp
+++ 프로그래머스/숫자 문자열과 영단어.cpp
@@ -1,24 +1,29 @@
 #include <string>
 #include <vector>
+#include <stdlib.h>
 
 using namespace std;
 
 int solution(string s) {
     int answer = 0;
+    string strAnswer = NULL;
     string number[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-    for(int i=0; i<strlen(s); i++){
-        if(s[i]>="a"&&s[i]<="z"){
+    for(int i=0; i<s.length(); i++){
+        if(s[i]>='a'&&s[i]<='z'){
             string temp;
-            for(int j=i; s[j]>="a"&&s[j]<="z"; j++){
-                temp = 
+            for(int j=i; s[j]>='a'&&s[j]<='z'; j++){
+                temp = s.substr(i, j);
                 for(int k=0; k<10; k++){
-                    if(strcmp(temp, number[k])==0){
+                    if(temp.compare(number[k]) == 0){
+                        strAnswer += number[j];
                         break;
                     }
                 }
-                
             }
+        }else{
+            strAnswer += s.at(i);
         }
     }
+    answer = stoi(strAnswer);
     return answer;
 }
