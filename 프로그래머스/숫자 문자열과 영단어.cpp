7e8f6f201f2d5bc05aebdf5a135b04a709084f56
Kim So Yeon <s2023@e-mirim.hs.kr>
2022-02-16 21:07:24 +0900

    Update 숫자 문자열과 영단어.cpp

--- 프로그래머스/숫자 문자열과 영단어.cpp
+++ 프로그래머스/숫자 문자열과 영단어.cpp
@@ -8,21 +8,29 @@ int solution(string s) {
     int answer = 0;
     string strAnswer = NULL;
     string number[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-    for(int i=0; i<s.length(); i++){
+    int i=0;
+    
+    //for(int i=0; i<s.length(); i++){
+    while(i < s.length()){
         if(s[i]>='a'&&s[i]<='z'){
             string temp;
+            bool bFor = true;
             for(int j=i; s[j]>='a'&&s[j]<='z'; j++){
+                if(!bFor) break;
                 temp = s.substr(i, j);
                 for(int k=0; k<10; k++){
                     if(temp.compare(number[k]) == 0){
-                        strAnswer += number[j];
+                        strAnswer.push_back(k);
+                        i = j;
+                        bFor = false;
                         break;
                     }
                 }
             }
         }else{
-            strAnswer += s.at(i);
+            strAnswer.push_back(s.at(i));
         }
+        i++; //***while
     }
     answer = stoi(strAnswer);
     return answer;
