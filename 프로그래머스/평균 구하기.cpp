47c48c82e1c42db674c8641a63e7551f5639953b
Kim So Yeon <s2023@e-mirim.hs.kr>
2022-03-26 20:13:19 +0900

    Create 평균 구하기.cpp
    https://programmers.co.kr/learn/courses/30/lessons/12944
    [필기]
    numeric: accumulate(start Index, end Index, sum init)

---
+++ 프로그래머스/평균 구하기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
+double solution(vector<int> arr) {
+    double answer = 0;
+    
+    answer = (double)accumulate(arr.begin(), arr.end(), 0) / arr.size();
+    
+    return answer;
+}
