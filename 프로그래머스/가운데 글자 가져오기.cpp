48802d4a2a970d268de5c86c66bc48b1061df471
Kim So Yeon <s2023@e-mirim.hs.kr>
2022-03-28 13:35:05 +0900

    Create 가운데 글자 가져오기.cpp
    https://programmers.co.kr/learn/courses/30/lessons/12903?language=cpp

---
+++ 프로그래머스/가운데 글자 가져오기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    int size = s.size();
+    if(size % 2 != 0){
+        answer.push_back(s[size/2]);
+    }else{
+        answer.push_back(s[size/2-1]);
+        answer.push_back (s[size/2]);
+    }
+    return answer;
+}
