30648d890b7b01d42e5f89da9f2a84162d9b377e
Kim So Yeon <s2023@e-mirim.hs.kr>
2022-03-28 13:53:02 +0900

    Create 문자열 내 마음대로 정렬하기.cpp
    https://programmers.co.kr/learn/courses/30/lessons/12915
    [필기]
    algorithm - sort() - compare()

---
+++ 프로그래머스/문자열 내 마음대로 정렬하기.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+
+bool compare(string a, string b){
+    if(a[N] == b[N]) return a<b;
+    else return a[N]<b[N];
+}
+
+vector<string> solution(vector<string> strings, int n) {
+    vector<string> answer = strings;
+    N = n;
+    
+    sort(answer.begin(), answer.end(), compare);
+    
+    return answer;
+}
