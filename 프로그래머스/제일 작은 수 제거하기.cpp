ba93a9dde31cba559ab53bfcee14178979885297
Kim So Yeon <s2023@e-mirim.hs.kr>
2022-03-30 15:02:50 +0900

    Create 제일 작은 수 제거하기.cpp
    https://programmers.co.kr/learn/courses/30/lessons/12935

---
+++ 프로그래머스/제일 작은 수 제거하기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr){
+    vector<int> answer;
+    if(arr.size() == 1) {
+        answer.push_back(-1);
+        return answer;
+    };
+    int min = *min_element(arr.begin(), arr.end());
+    for(int i=0; i<arr.size(); i++){
+        if(arr[i] != min) answer.push_back(arr[i]); 
+    }
+    return answer;
+}
